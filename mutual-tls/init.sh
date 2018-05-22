76b7faba05c613094d3f92e158f9cf8181f2839a
j987987 <j987897@users.noreply.github.com>
2018-05-22 12:56:18 -0400

    client gen

--- mutual-tls/init.sh
+++ mutual-tls/init.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+
 echo ============= Create a CA for the server cert =============
 ./create_ca.sh ca_server
 
 echo ============= Create a server cert =============
 ./create_server.sh ca_server
 
-echo ============= Create a CA for the server cert =============
+echo ============= Create a CA for the client cert =============
 ./create_ca.sh ca_client
 
-echo ============= Create a server cert =============
-./create_client.sh ca_client snort
+echo ============= Create a client cert =============
+./create_client.sh ca_client testclient
+
+cp server-key.pem ../certs
+cp server-crt.pem ../certs
+cp ca_client-crt.pem ../certs
+cp ca_client-crt.srl ../certs
